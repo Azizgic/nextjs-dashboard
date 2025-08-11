a5a54128829ffe262d10dbe96bfefeeac373da67
Azizgic <azizgichki3@gmail.com>
2025-08-12 01:39:19 +0500

    nothing

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-    return (<p>Dashboard Page</p>)
+import RevenueChart from "@/app/ui/dashboard/revenue-chart"
+import { lusitana } from "@/app/ui/fonts"
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data"
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+
+export default async function Page() {
+    const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices();
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+            <div></div>
+            <div className="mt-6 grid gird-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                {<RevenueChart revenue={revenue} />}
+                {<LatestInvoices latestInvoices={latestInvoices} />}
+            </div>
+        </main>
+    )
 }
